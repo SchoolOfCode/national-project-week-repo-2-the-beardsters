c63cfd4bed1fc5ec4384cfe8daaf50893751f250
Luke Chaplin <93163215+lukechaplin@users.noreply.github.com>
2022-01-19 12:10:04 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from '../Navbar/navbar.js'
 
 function App() {
+
+    async function getData() {
+      const res = await fetch("https://soc-challenge-generator.herokuapp.com/backend");
+      const data = await res.json();
+      console.log(data);
+      return data;
+    }
+    const result =  getData();
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div>result</div>
     </div>
   );
 }
