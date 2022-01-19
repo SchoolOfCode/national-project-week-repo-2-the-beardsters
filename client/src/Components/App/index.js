4861d2cd5a1e2cdc2f6e6b0e781d59d61d9791ce
marian.eq <mariano.eq@gmail.com>
2022-01-19 10:32:02 -0300

    button added in UserInput

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -1,19 +1,22 @@
-import './App.css';
-import Navbar from '../Navbar/navbar.js'
+import "./App.css";
+import Navbar from "../Navbar/navbar.js";
+import UserInput from "../UserInput/UserInput";
 
 function App() {
+  async function getData() {
+    const res = await fetch(
+      "https://soc-challenge-generator.herokuapp.com/backend"
+    );
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }
+  const result = getData();
 
-    async function getData() {
-      const res = await fetch("https://soc-challenge-generator.herokuapp.com/backend");
-      const data = await res.json();
-      console.log(data);
-      return data;
-    }
-    const result =  getData();
-    
   return (
     <div className="App">
       <Navbar></Navbar>
+      <UserInput />
     </div>
   );
 }
---
+++ client/src/Components/UserInput/UserInput.js
@@ -0,0 +1,5 @@
+const UserInput = () => {
+  return <button>Get your challenge</button>;
+};
+
+export default UserInput;
