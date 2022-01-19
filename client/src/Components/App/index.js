acc3c7debb1e794f05fbfb9ed8a86969f4c1c2df
marian.eq <mariano.eq@gmail.com>
2022-01-19 15:12:43 -0300

    Fix broken App component

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -1,32 +1,23 @@
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "../Navbar/navbar.js";
-<<<<<<< HEAD
 import Userchoice from "../Userchoice/userchoice.js";
 import UserAnswer from "../UserAnswer";
 import ChallengeDesc from "../ChallengeDesc";
-import React, {useState} from 'react'
-||||||| 4424c01
-import UserInput from "../UserInput/UserInput";
-=======
 import Darkmode from "../Darkmode/darkmode";
->>>>>>> ca65c7555c38619e2675c68b88d4dc877836cff0
 
 function App() {
-const [message, setMessage] = useState("message");
-const [answer, setAnswer] = useState("");
-
+  const [message, setMessage] = useState("message");
+  const [answer, setAnswer] = useState("");
 
   async function getData() {
-
-    const res = await fetch(
-      "https://soc-challenge-generator.herokuapp.com/"
-    );
+    const res = await fetch("https://soc-challenge-generator.herokuapp.com/");
     const data = await res.json();
     console.log(data);
-    setMessage(data.payload[0])
+    setMessage(data.payload[0]);
     return data;
   }
- 
+
   function getUserAnswer(e) {
     setAnswer(e.target.value);
     console.log(answer);
@@ -34,27 +25,28 @@ const [answer, setAnswer] = useState("");
 
   function submitFunction(e) {
     if (answer === message.answer) {
-      console.log(message.answer)
-      console.log("Right answer")
+      console.log(message.answer);
+      console.log("Right answer");
     } else {
-      console.log(message.answer)
-      console.log("wrong answer")}
+      console.log(message.answer);
+      console.log("wrong answer");
+    }
   }
 
   return (
     <div className="App" id="home">
-
       <Navbar />
-      <Userchoice  onClick={() => getData()} />
-    <ChallengeDesc content={message} />
-        <details name="See answer" value="See answer" placeholder="see answer">
+      <Userchoice onClick={() => getData()} />
+      <ChallengeDesc content={message} />
+      <details name="See answer" value="See answer" placeholder="see answer">
         <summary>See Answer</summary>
-        <div>          {message.answer}</div>
-        </details>
-      <UserAnswer onClick={(e) => submitFunction(e)} onChange={(e) => getUserAnswer(e)}/>
+        <div> {message.answer}</div>
+      </details>
+      <UserAnswer
+        onClick={(e) => submitFunction(e)}
+        onChange={(e) => getUserAnswer(e)}
+      />
       <Darkmode />
-
-
     </div>
   );
 }
