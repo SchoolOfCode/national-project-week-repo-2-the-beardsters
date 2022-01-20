38ce8c2626da892b73b2408f2449003005af5463
lukechaplin <lukechaplin@gmail.com>
2022-01-20 12:04:43 +0000

    attempted input component fix did not work reverted to original

--- client/src/Components/UserAnswer/index.js
+++ client/src/Components/UserAnswer/index.js
@@ -1,10 +1,11 @@
-export default function UserAnswer(userResponse) {
+export default function UserAnswer(userResponse, text) {
   return (
     <form action="#">
       <input
         type="text"
         placeholder="Add your answer here"
         onChange={userResponse.onChange}
+        value={text}
       />
 
       <button type="submit" value="Submit" onClick={userResponse.onClick}>
